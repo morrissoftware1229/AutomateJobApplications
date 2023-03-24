17f32d3fb307eae1081415383865f75e8ca3dc96
Charles Morris <morrissoftware1229@gmail.com>
2023-03-23 22:04:43 -0500

    Added code to press search button and tried to add code to filter for remote

--- AutomateJobApplications.py
+++ AutomateJobApplications.py
@@ -4,6 +4,8 @@ import pyautogui as gui
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
 
 #Importing this because location bar won't clear out using clear()
@@ -19,6 +21,7 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 #locationToSearchIn = input("Please enter the location you would like to check in")
 #pathToResume = input("Please enter absolute path to resume")
 #remoteOption = input("Please enter 1 for remote or 0 for in-person")
+#print(remoteOption)
 
 #Navigates to Indeed.com
 driver.get("https://www.indeed.com/")
@@ -35,3 +38,17 @@ searchWhereBar = driver.find_element(value="text-input-where")
 searchWhereBar.send_keys(Keys.CONTROL + "a")
 searchWhereBar.send_keys(Keys.DELETE)
 searchWhereBar.send_keys('United States')
+
+#Presses enter button
+enterButton = driver.find_element(By.XPATH, "//button[@type='submit']")
+enterButton.click()
+
+#Sets jobs to remote if indicated by user
+#if(remoteOption == 1):
+    #remoteButton = Select(driver.find_element("filter-remotejob"))
+    #remoteButton.select_by_visible_text("Remote")
+    #remoteOption = driver.find_element("filter-remotejob-0")
+    #remoteOption.click()
+    #enterRemoteButton = driver.find_element(By.XPATH, "//button[@type='submit']")
+    #enterRemoteButton.click()
+
